0b6de3173790cef05b3c3978f6befcecbdcbf5e5
Jelvig <111405495+Jelvig@users.noreply.github.com>
2022-11-15 12:57:12 -0800

    Update bins.py

--- bins.py
+++ bins.py
@@ -10,8 +10,8 @@ class Bins():
   def create_entry_widget(df):
     for bin in range(len(df.index/96))
       Bins.entry.grid(row=0, column=bin, pady=20, padx=5)
-      self.bin_entries.append(entry)
-    self.bin_entries.sort()
+      Bins.bin_entries.append(entry)
+    Bins.bin_entries.sort()
     
   @classmethod 
   def get_bins(cls):
