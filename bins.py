1c4f0de4d7bcf0f4bac927f9965fe79cf661f60c
Jelvig <111405495+Jelvig@users.noreply.github.com>
2022-11-15 11:59:11 -0800

    Update bins.py

--- bins.py
+++ bins.py
@@ -1,10 +1,19 @@
 class Bins():
+  from tkinter import *
+  root = Tk()
+  root.title("Enter Bin Codes")
+  root.geometry("850x900")
+  root.iconbitmap('/icon.ico')
+  entry = Entry(root)
+  
+  def '__init__'(self)
+    self.bin_entries = []
+    
   def create_entry_widget(df):
-    # Import the Tkinter Library
-    import tkinter as tk
-    root = tk.Tk()
-    root.title("Enter Bin Codes")
-    root.geometry("850x500")
-    root.iconbitmap('/icon.ico')
-    entry = tk.Entry(root)
-    entry.grid(row=len(df.index), column=0, pady=20, padx=9)
+    for bin in range(len(df.index/96))
+      Bins.entry.grid(row=0, column=bin, pady=20, padx=5)
+      self.bin_entries.append(entry)
+    self.bin_entries.sort()
+  @classmethod 
+  def get_bins(self):
+    return [x for x in self.bin_entries]
