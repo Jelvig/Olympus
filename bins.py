d479fa5383e5d77fcd2a036777bae6c95f1bffaf
Jelvig <111405495+Jelvig@users.noreply.github.com>
2022-11-15 12:48:29 -0800

    Update bins.py

--- bins.py
+++ bins.py
@@ -5,15 +5,14 @@ class Bins():
   root.geometry("850x900")
   root.iconbitmap('/icon.ico')
   entry = Entry(root)
-  
-  def '__init__'(self)
-    self.bin_entries = []
+  bin_entries = []
     
   def create_entry_widget(df):
     for bin in range(len(df.index/96))
       Bins.entry.grid(row=0, column=bin, pady=20, padx=5)
       self.bin_entries.append(entry)
     self.bin_entries.sort()
+    
   @classmethod 
-  def get_bins(self):
-    return [x for x in self.bin_entries]
+  def get_bins(cls):
+    return [x for x in Bin.bin_entries]
