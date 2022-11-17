a54d622e2c502b7b9149f02e0bd6818685304169
Jelvig <111405495+Jelvig@users.noreply.github.com>
2022-11-17 12:43:29 -0800

    Update PRC.py

--- Engine/PRC.py
+++ Engine/PRC.py
@@ -1,7 +1,8 @@
-from Engine import bins
-from Engine import working_folder
+from .bins import Bins
+from .working_folder import Working_folder
+from .extract import Extract
 
-class PRC():
+class PRC(Bins, Working_folder, Extract):
     def '__init__'(self, file_loc, files)
         self.file_loc = file_loc
         self.files = files
@@ -13,7 +14,7 @@ class PRC():
         conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\jelvig\Desktop\Poseidon_Test.accdb;')
         cursor = conn.cursor()
 
-        # will insert values as list to find
+        # will insert values as list to find all
         placeholder = '?'
         placeholder = ', '.join(placeholder for unused in items_list)
 
@@ -35,41 +36,17 @@ class PRC():
         upload = df.insert(loc=3,column="tobincode", values=serialize(df, bin_codes))
         return upload
 
-        # def get_bins(df):
-        #   from math import ceil
-        #   rows = len(df.index)
-        #   bin_count = ceil(rows/96)
-        #   bincodes = []
-        #   i=0
-        #   while i <= bin_count:
-        #     bin_code = input(f"Enter bin code {i}/{bin_count}:")
-        #     if len(bin_code) == 9:
-        #       bincodes.append(bin_code) 
-        #       i += 1
-        #     else:
-        #       print("This is an invalid bin code, try again")
-        #   return bincodes
-
-        # def file_list():
-        #     from tkinter import filedialog
-        #     from pandas import read_csv
-
-        #     file = filedialog.askopenfilename(title="Choose master file", initialdir=r'W:\Production\Probe Oligos\REMP Files\_Re-Rack Files')
-        #     item_df = read_csv(file, header=None, index_col=None)
-        #     item_list = item_df[1].tolist()
-        #     return item_list
-
     def serialize(df, bin_codes):
         letters = ['A','B','C','D','E','F','G','H']
         i=0
         while i <= len(df.index):
-        for bin_code in bin_codes:  
-        for num in range(13):
-        for let in letters:
-        f"{bin_code}-{let}{num}"
+            for bin_code in bin_codes:  
+                for num in range(13):
+                    for let in letters:
+                        f"{bin_code}-{let}{num}"
 
     def main():
         item_list = file_list()
         df = query_items(item_list)
         upload = sort_write(df)
-        upload.to_csv('location', index=False, header = False)
+        upload.to_csv(file_loc'/upload', index=False, header = False)
