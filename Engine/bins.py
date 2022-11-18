0808bb432027af4f408c5ff2ec19420dfa60b7d4
Jelvig <111405495+Jelvig@users.noreply.github.com>
2022-11-18 14:27:10 -0800

    Update bins.py

--- Engine/bins.py
+++ Engine/bins.py
@@ -10,16 +10,27 @@ class Bins():
         textBox.pack()
         label = tk.Label(root, text=f'Enter {ceil(self.df/96)} Bins').pack()
         buttonCommit=tk.Button(root, height=1, width=10, text="Commit", 
-                            command=lambda: retrieve_input(textBox))
+                            command=lambda: retrieve_input(textBox, root))
         buttonCommit.pack()
         tk.mainloop()
+        res = check_values(inputValue)
+        return res
 
-def retrieve_input(textBox):
+    def check_values(inputValue):
+        from math import ceil
+        if len(inputValue) == ceil(self.df.index):
+            if all(len(bin) == 9 for bin in inputValue):
+                return inputValue
+            else:
+                print('Improper bin was typed, please reinput')
+                self.bin_commit()
+        else:
+            print('Insufficient number of bins, please re-enter')
+            self.bin_commit()
+
+def retrieve_input(textBox, root):
     """ add in a check to make sure the list is the correct length of bins,
     and then check to make sure the bins are accurate, or call bin commit again."""
+    global inputValue
     inputValue=textBox.get("1.0","end-2c").split('\n')
-    print(inputValue)
-
-df = 1500
-bin = Bins(df)
-bin.bin_commit()
+    root.destroy()
