2a2acc39e37b0628452ec50269373b9049d9d6fa
Jelvig <111405495+Jelvig@users.noreply.github.com>
2022-11-18 12:11:16 -0800

    Update bins.py

--- Engine/bins.py
+++ Engine/bins.py
@@ -1,23 +1,25 @@
 class Bins():
-  from tkinter import *
-  def '__init__'(self, df)
-    self.df = df
-    self.root = Tk()
-    self.root.title("Enter Bin Codes")
-    self.root.geometry("850x900")
-    self.root.iconbitmap('/icon.ico')
-    self.entry = Entry(root)
-    self.bin_entries = []
-  
-  def create_entry_widget():
-    from math import ceil
-    bin = ceil(self.df.index/96)
-    self.entry.grid(row=0, column=bin, pady=20, padx=5)
-    my_button = Button(self.root, text='Append Bin', command=add_button)
-    my_button.grid(row=1,column=0,pady=20)
-    my_label = Label(root, text='Bin Selection')
-    my_label.grid(row=2, column=0, pady=20)
-    root.mainloop()
-                  
-  def add_button(self):
-    self.bin_entries.append(self.entry)
+    def __init__(self, df):
+        self.df = df
+
+    def bin_commit(self):
+        import tkinter as tk
+        from math import ceil
+        root=tk.Tk()
+        textBox=tk.Text(root, height=10, width=20)
+        textBox.pack()
+        label = tk.Label(root, text=f'Enter {ceil(self.df/96)} Bins').pack()
+        buttonCommit=tk.Button(root, height=1, width=10, text="Commit", 
+                            command=lambda: retrieve_input(textBox))
+        buttonCommit.pack()
+        tk.mainloop()
+
+def retrieve_input(textBox):
+    """ add in a check to make sure the list is the correct length of bins,
+    and then check to make sure the bins are accurate, or call bin commit again."""
+    inputValue=textBox.get("1.0","end-2c").split('\n')
+    print(inputValue)
+
+df = 1500
+bin = Bins(df)
+bin.bin_commit()
