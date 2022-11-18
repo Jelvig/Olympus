289bd69d629f101b352ca142f5107d43dfff7c4e
Jelvig <111405495+Jelvig@users.noreply.github.com>
2022-11-18 12:12:45 -0800

    Update extract.py

--- Engine/extract.py
+++ Engine/extract.py
@@ -2,8 +2,8 @@ class Extract():
   def '__init__'(self, files):
     self.files = files
   
-  def extract(cls):
+  def extract(self):
     import pandas as pd
-    df = pd.read_csv(files, usecols=[1],header=None,index_col=None)
+    df = pd.read_csv(self.files, usecols=[1],header=None,index_col=None)
     item_list = df.values.tolist()
     return item_list
