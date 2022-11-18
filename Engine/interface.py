4a5b73a869e99fe4acd85b60a8b0e9414f68cdd6
Jelvig <111405495+Jelvig@users.noreply.github.com>
2022-11-18 14:29:20 -0800

    Update interface.py

--- Engine/interface.py
+++ Engine/interface.py
@@ -1,7 +1,10 @@
 
 
 
-class Interface(PRC,LVR):
+class Interface():
+  def __init__(self, PRC, LVR):
+    self.PRC = PRC
+    self.LVR = LVR
   def window():
     import tkinter as tk
 
@@ -9,7 +12,7 @@ class Interface(PRC,LVR):
     win.geometry("600x250")
     win.resizable(False, False)
 
-    tk.Button(win, text= "Pre-Rack Condenstation",fg='black',height=5, width=30, command=quit).pack()
-    tk.Button(win, text= "Low-Volume Removal", fg='black',height=5, width=30, command=write_text).pack()
+    tk.Button(win, text= "Pre-Rack Condenstation",fg='black',height=5, width=30, command=self.PRC).pack()
+    tk.Button(win, text= "Low-Volume Removal", fg='black',height=5, width=30, command=self.LVR).pack()
     win.mainloop()
 
