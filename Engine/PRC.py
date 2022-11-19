ae2d1efaf71223d1a76b121171ea4eda23335c11
Jelvig <111405495+Jelvig@users.noreply.github.com>
2022-11-19 07:24:06 -0800

    Update PRC.py

--- Engine/PRC.py
+++ Engine/PRC.py
@@ -6,28 +6,7 @@ class PRC(Bins, Working_folder, Extract):
     def '__init__'(self, file_loc, files)
         self.file_loc = file_loc
         self.files = files
-        
-    def query_items(items_list:list):
-        import pyodbc
-        import pandas as pd
-
-        conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\jelvig\Desktop\Poseidon_Test.accdb;')
-        cursor = conn.cursor()
-
-        # will insert values as list to find all
-        placeholder = '?'
-        placeholder = ', '.join(placeholder for unused in items_list)
-
-        data = """
-        SELECT qy_oligos_containers.[Item No_], qy_oligos_containers.[Lot No_], qy_oligos_containers.[Bin Code], '' AS toBinCode, qy_oligos_containers.lotQty AS Qty, 'UL' AS UOMC, qy_oligos_containers.type
-        FROM ((qy_oligos_containers INNER JOIN [dbo_NanoString$Bin Content] ON qy_oligos_containers.[Bin Code] = [dbo_NanoString$Bin Content].[Bin Code]) INNER JOIN qy_rack_contents ON qy_oligos_containers.shelfCode = qy_rack_contents.shelfCode) INNER JOIN [dbo_NanoString$Item] ON [dbo_NanoString$Bin Content].[Item No_] = [dbo_NanoString$Item].No_
-        WHERE (qy_oligos_containers.[Item No_] IN (%s))
-        ORDER BY qy_oligos_containers.[Lot No_];""" % placeholder
-
-        cursor.execute(data, items_list)
-        items = cursor.fetchall()
-        df = pd.DataFrame(items, columns=['Item', 'Lot', 'Bin code', 'Qty', 'UL'])
-        return df
+       
 
     def sort_write(df):
         df.drop_duplicates(subset=['Item'], keep='first')
