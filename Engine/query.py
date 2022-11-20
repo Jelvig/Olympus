56e8844d200dfeac95184b71eb931774c8bc1e4c
Jelvig <111405495+Jelvig@users.noreply.github.com>
2022-11-20 08:57:24 -0800

    Update query.py

--- Engine/query.py
+++ Engine/query.py
@@ -11,14 +11,63 @@ class Query:
     from pandas import DataFrame
     
     placeholder = '?'
-    placeholder = ', '.join(placeholder for unused in self.items_list)
+    placeholder = ', '.join(placeholder for unused in self.item_list)
     data = """
     SELECT qy_oligos_containers.[Item No_], qy_oligos_containers.[Lot No_], qy_oligos_containers.[Bin Code], '' AS toBinCode, qy_oligos_containers.lotQty AS Qty, 'UL' AS UOMC, qy_oligos_containers.type
     FROM ((qy_oligos_containers INNER JOIN [dbo_NanoString$Bin Content] ON qy_oligos_containers.[Bin Code] = [dbo_NanoString$Bin Content].[Bin Code]) INNER JOIN qy_rack_contents ON qy_oligos_containers.shelfCode = qy_rack_contents.shelfCode) INNER JOIN [dbo_NanoString$Item] ON [dbo_NanoString$Bin Content].[Item No_] = [dbo_NanoString$Item].No_
     WHERE qy_oligos_containers.type=2 AND (qy_oligos_containers.[Item No_] IN (%s))
     ORDER BY qy_oligos_containers.[Lot No_];""" % placeholder
 
-    self.cursor.execute(data, self.items_list)
+    self.cursor.execute(data, self.item_list)
     items = self.cursor.fetchall()
     df = DataFrame(items, columns=['Item', 'Lot', 'Bin code', 'Qty', 'UL'])
+    return df
+  
+  def lot_query(self):
+    import pyodbc
+    from pandas import DataFrame
+    
+    placeholder = '?'
+    placeholder = ', '.join(placeholder for unused in self.lot_list)
+    data = """
+    SELECT qy_oligos_containers.[Item No_], qy_oligos_containers.[Lot No_], qy_oligos_containers.[Bin Code], '' AS toBinCode, qy_oligos_containers.lotQty AS Qty, 'UL' AS UOMC, qy_oligos_containers.type
+    FROM ((qy_oligos_containers INNER JOIN [dbo_NanoString$Bin Content] ON qy_oligos_containers.[Bin Code] = [dbo_NanoString$Bin Content].[Bin Code]) INNER JOIN qy_rack_contents ON qy_oligos_containers.shelfCode = qy_rack_contents.shelfCode) INNER JOIN [dbo_NanoString$Item] ON [dbo_NanoString$Bin Content].[Item No_] = [dbo_NanoString$Item].No_
+    WHERE (qy_oligos_containers.[Lot No_] IN (%s))
+    ORDER BY qy_oligos_containers.[Lot No_];""" % placeholder
+
+    self.cursor.execute(data, self.lot_list)
+    lots = self.cursor.fetchall()
+    df = DataFrame(lots, columns=['Item', 'Lot', 'Bin code', 'Qty', 'UL'])
+    return df
+  
+  def altlot_query(self, items):
+    import pyodbc
+    from pandas import DataFrame
+    
+    placeholder = '?'
+    placeholder = ', '.join(placeholder for unused in items)
+    data = """
+    SELECT qy_oligos_containers.[Item No_], qy_oligos_containers.[Lot No_], qy_oligos_containers.[Bin Code], '' AS toBinCode, qy_oligos_containers.lotQty AS Qty, 'UL' AS UOMC, qy_oligos_containers.type
+    FROM ((qy_oligos_containers INNER JOIN [dbo_NanoString$Bin Content] ON qy_oligos_containers.[Bin Code] = [dbo_NanoString$Bin Content].[Bin Code]) INNER JOIN qy_rack_contents ON qy_oligos_containers.shelfCode = qy_rack_contents.shelfCode) INNER JOIN [dbo_NanoString$Item] ON [dbo_NanoString$Bin Content].[Item No_] = [dbo_NanoString$Item].No_
+    WHERE qy_oligos_containers.type=2 AND (qy_oligos_containers.[Item No_] IN (%s))
+    ORDER BY qy_oligos_containers.[Lot No_];""" % placeholder
+
+    self.cursor.execute(data, items)
+    alt_lotlist = self.cursor.fetchall()
+    df = DataFrame(alt_lotlist, columns=['Item', 'Lot', 'Bin code', 'Qty', 'UL'])
+    return df
+  
+    def lowvol_query(self):
+    import pyodbc
+    from pandas import DataFrame
+    
+    data = """
+    SELECT qy_oligos_containers.[Item No_], qy_oligos_containers.[Lot No_], qy_oligos_containers.[Bin Code], '' AS toBinCode, qy_oligos_containers.lotQty AS Qty, 'UL' AS UOMC, qy_oligos_containers.type
+    FROM ((qy_oligos_containers INNER JOIN [dbo_NanoString$Bin Content] ON qy_oligos_containers.[Bin Code] = [dbo_NanoString$Bin Content].[Bin Code]) INNER JOIN qy_rack_contents ON qy_oligos_containers.shelfCode = qy_rack_contents.shelfCode) INNER JOIN [dbo_NanoString$Item] ON [dbo_NanoString$Bin Content].[Item No_] = [dbo_NanoString$Item].No_
+    WHERE (qy_oligos_containers.type=2 OR 3) AND Qty < 40
+    ORDER BY qy_oligos_containers.[Lot No_];"""
+
+    self.cursor.execute(data, self.item_list)
+    lowvol_list = self.cursor.fetchall()
+    df = DataFrame(lowvol_list, columns=['Item', 'Lot', 'Bin code', 'Qty', 'UL'])
     return df
