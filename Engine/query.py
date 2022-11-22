1e4113e2b078ac63fc01793176e12717c4a670c9
Jelvig <111405495+Jelvig@users.noreply.github.com>
2022-11-21 16:41:43 -0800

    Update query.py

--- Engine/query.py
+++ Engine/query.py
@@ -41,19 +41,19 @@ class Query:
     df = DataFrame(alt_lotlist, columns=['Item', 'Lot', 'Bin code', 'Qty', 'UL'])  # return just items for easy comparison? delete apart of the query?
     return df
   
-    def lowvol_query(self):
+    def lowvol_query(self, lot_list):
       import pyodbc
       from pandas import DataFrame
 
       placeholder = '?'
-      placeholder = ', '.join(placeholder for unused in self.lot_list)
+      placeholder = ', '.join(placeholder for unused in lot_list)
       data = """
       SELECT qy_oligos_containers.[Item No_], qy_oligos_containers.[Lot No_], qy_oligos_containers.[Bin Code], '' AS toBinCode, qy_oligos_containers.lotQty AS Qty, 'UL' AS UOMC, qy_oligos_containers.type
       FROM ((qy_oligos_containers INNER JOIN [dbo_NanoString$Bin Content] ON qy_oligos_containers.[Bin Code] = [dbo_NanoString$Bin Content].[Bin Code]) INNER JOIN qy_rack_contents ON qy_oligos_containers.shelfCode = qy_rack_contents.shelfCode) INNER JOIN [dbo_NanoString$Item] ON [dbo_NanoString$Bin Content].[Item No_] = [dbo_NanoString$Item].No_
       WHERE ((qy_oligos_containers.type=2 OR 3) AND Qty < 40) OR (qy_oligos_containers.[Lot No_] IN (%s))
       ORDER BY qy_oligos_containers.[Lot No_];""" % placeholder
 
-      self.cursor.execute(data, self.lot_list)
+      self.cursor.execute(data, lot_list)
       lowvol_list = self.cursor.fetchall()
       df = DataFrame(lowvol_list, columns=['Item', 'Lot', 'Bin code', 'Qty', 'UL'])
       return df
