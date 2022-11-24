444ec3dc284deaa09c8d8b02beabce4c47069124
Jelvig <111405495+Jelvig@users.noreply.github.com>
2022-11-24 10:58:31 -0800

    Update query.py

--- Engine/query.py
+++ Engine/query.py
@@ -23,23 +23,20 @@ class Query:
     df = DataFrame(items, columns=['Item', 'Lot', 'Bin code', 'Qty', 'UL'])
     return df
   
-  def altlot_query(self, order_list):
+  def altlot_query(self, item):
     import pyodbc
     from pandas import DataFrame
-    
-    placeholder = '?'
-    self.item_list = ', '.join(placeholder for unused in self.items_list)
-    lot = ', '.join(placeholder for unused in order_list)
-    data = """
-    SELECT qy_oligos_containers.[Item No_], qy_oligos_containers.[Lot No_], qy_oligos_containers.[Bin Code], '' AS toBinCode, qy_oligos_containers.lotQty AS Qty, 'UL' AS UOMC, qy_oligos_containers.type
-    FROM ((qy_oligos_containers INNER JOIN [dbo_NanoString$Bin Content] ON qy_oligos_containers.[Bin Code] = [dbo_NanoString$Bin Content].[Bin Code]) INNER JOIN qy_rack_contents ON qy_oligos_containers.shelfCode = qy_rack_contents.shelfCode) INNER JOIN [dbo_NanoString$Item] ON [dbo_NanoString$Bin Content].[Item No_] = [dbo_NanoString$Item].No_
-    WHERE (qy_oligos_containers.[Item No_] IN (%s)) AND (qy_oligos_containers.[Lot No_] <> (%s))
-    ORDER BY qy_oligos_containers.[Lot No_];""" % self.item_list, lot
 
-    self.cursor.execute(data, (self.item_list, lot))
+    data = """
+    SELECT qy_oligos_containers.[Item No_], qy_oligos_containers.[Lot No_]
+    FROM (qy_oligos_containers)
+    WHERE (qy_oligos_containers.[Item No_] IN (%s))
+    ORDER BY qy_oligos_containers.[Lot No_];""" % item
+
+    self.cursor.execute(data, item)
     alt_lotlist = self.cursor.fetchall()
-    df = DataFrame(alt_lotlist, columns=['Item', 'Lot', 'Bin code', 'Qty', 'UL'])  # return just items for easy comparison? delete apart of the query?
-    return df
+   
+    return alt_lotlist
   
     def lowvol_query(self, lot_list):
       import pyodbc
