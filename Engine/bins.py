cf5ef0b580efc4220113b692815d235184bf98a9
Jelvig <111405495+Jelvig@users.noreply.github.com>
2022-11-24 07:59:03 -0800

    Update bins.py

--- Engine/bins.py
+++ Engine/bins.py
@@ -1,3 +1,6 @@
+"""final class needs df to be indexed,
+users must leave a new line"""
+
 class Bins():
     def bin_commit(self, df):
         import tkinter as tk
@@ -5,25 +8,25 @@ class Bins():
         root=tk.Tk()
         textBox=tk.Text(root, height=10, width=20)
         textBox.pack()
-        label = tk.Label(root, text=f'Enter {ceil(df.index/96)} Bins').pack()
+        label = tk.Label(root, text=f'Enter {ceil(df/96)} Bins').pack()
         buttonCommit=tk.Button(root, height=1, width=10, text="Commit", 
                             command=lambda: retrieve_input(textBox, root))
         buttonCommit.pack()
         tk.mainloop()
-        res = check_values(inputValue)
+        res = self.check_values(inputValue, df)
         return res
 
-    def check_values(inputValue, df):
+    def check_values(self, inputValue, df):
         from math import ceil
-        if len(inputValue) >= ceil(df.index/96):
+        if len(inputValue) >= ceil(df/96):
             if all(len(bin) == 9 for bin in inputValue):
                 return inputValue
             else:
                 print('Improper bin was typed, please reinput')
-                self.bin_commit()
+                self.bin_commit(df)
         else:
             print('Insufficient number of bins, please re-enter')
-            self.bin_commit()
+            self.bin_commit(df)
 
 def retrieve_input(textBox, root):
     """ add in a check to make sure the list is the correct length of bins,
@@ -31,3 +34,4 @@ def retrieve_input(textBox, root):
     global inputValue
     inputValue=textBox.get("1.0","end-2c").split('\n')
     root.destroy()
+
