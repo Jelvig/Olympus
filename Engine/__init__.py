d1b1c4c12784fbb027a050fd64a360728c35c6d3
Jelvig <111405495+Jelvig@users.noreply.github.com>
2022-12-16 06:33:57 -0800

    Update __init__.py

--- Engine/__init__.py
+++ Engine/__init__.py
@@ -1,6 +1,4 @@
 from .operator import Operator
-from .interface import Interface
-from .bins import Bins
 from .working_folder import Files
 from .query import Query
-from .lot import Lot
+from .lots import Lots
