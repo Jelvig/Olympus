e9d42e02b72b10dd4d6776e396cd82d7e38727c2
Jelvig <111405495+Jelvig@users.noreply.github.com>
2022-12-16 06:34:40 -0800

    Update lots.py

--- Engine/lots.py
+++ Engine/lots.py
@@ -1,10 +1,12 @@
 class Lots():
-    """needs updates
-    This class is purely for gathering lots that are located
-    on the garabge wall, without manuelling entering them"""
-    def get_lots():
-        import pandas as pd
-        garbage_wall = pd.read_excel('path')
-        lots = garbage_wall[garbage_wall['removed?'] != False]
-        lots = lots["lot number?"].values.tolist()
-        return lots
+    """ This class is purely for gathering lots that are located
+        on the garabge wall, without manualling entering them"""
+    def get_lots(self):
+            import pandas as pd
+            import openpyxl
+            
+            garbage_wall = pd.read_excel(r"W:\Production\Probe Oligos\REMP Files\_Re-Rack Files\Garbage Wall.xlsx", sheet_name='Garbage Wall', engine='openpyxl')
+            lots = garbage_wall[(garbage_wall['Removed From NAV'].isnull())]
+            lots = lots["Lot #"].values.tolist()
+            lots = [int(x) for x in lots]
+            return lots
