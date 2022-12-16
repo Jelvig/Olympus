50343c1af23f18c606456bdca030266768a8dedb
Jelvig <111405495+Jelvig@users.noreply.github.com>
2022-12-16 06:35:39 -0800

    Update working_folder.py

--- Engine/working_folder.py
+++ Engine/working_folder.py
@@ -1,11 +1,15 @@
-  class files:
-    def file_list():
+class Files:
+    def file_list(self):
+        """By choosing the working folder, it gathers the master files, and location of folder"""
         from tkinter import filedialog
         import glob
-        While True:
-          file_loc = filedialog.askdirectory(title="Choose Working Folder", initialdir=r'W:\Production\Probe Oligos\REMP Files\_Re-Rack Files')
-          files = glob.glob(file_loc+"/*_opf.csv")
-          if files:
-            return file_loc, files
-          else:
-            print("No master files were found, try again")
+        while True:
+            file_loc = filedialog.askdirectory(title="Choose Working Folder", initialdir=r'W:\Production\Probe Oligos\REMP Files\_Re-Rack Files')
+            files = list(glob.glob(file_loc+"/*_opf.csv"))
+            files.reverse()
+
+            if files != None:
+                return file_loc, files
+            else:
+                print("No master files were found, Exiting program")
+                exit()
